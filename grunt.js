82541c9664dd23e67bf5a286469a2b3faaf60405
Tyler Kellen <tyler@sleekcode.net>
2012-09-04 13:06:58 -0500

    tinkering with testing

--- grunt.js
+++ grunt.js
@@ -1,10 +1,14 @@
 module.exports = function(grunt) {
   "use strict";
 
+  grunt.file.mkdir("tmp");
+
   grunt.initConfig({
+
     lint: {
       all: ["grunt.js", "tasks/*.js"]
     },
+
     jshint: {
       options: {
         curly: true,
@@ -20,9 +24,35 @@ module.exports = function(grunt) {
         node: true,
         es5: true
       }
+    },
+
+    clean: {
+      tmp: ["tmp"]
+    },
+
+    less: {
+      compile: {
+        files: {
+          "tmp/less_a.css": ["test/fixtures/style.less"],
+          "tmp/less_b.css": ["test/fixtures/style.less"],
+          "tmp/less_c.css": ["test/fixtures/**/*.nomatches"],
+          "tmp/less_d.css": ["test/fixtures/style.less", "test/fixtures/style2.less"]
+        },
+        options: {
+          paths: ["test/fixtures/include"]
+        }
+      }
+    },
+
+    test: {
+      tasks: ["test/*_test.js"]
     }
+
   });
 
   grunt.registerTask("default", "lint");
+  //grunt.renameTask("test", "test2");
+  //grunt.registerTask("test", "clean less test2 clean");
   grunt.loadTasks("tasks");
 };
+
