e55af3e2bc3a869f2518e5ae31b47fd3a85bc3dd
Ben Alman <cowboy@rj3.net>
2012-09-05 18:04:54 -0400

    Proposed unit testing setup. Should probably equate task targets to test modules, though.

--- grunt.js
+++ grunt.js
@@ -1,12 +1,11 @@
 module.exports = function(grunt) {
-  "use strict";
+  'use strict';
 
+  // Project configuration.
   grunt.initConfig({
-
     lint: {
-      all: ["grunt.js", "tasks/*.js"]
+      all: ['grunt.js', 'tasks/*.js', '<config:nodeunit.tasks>']
     },
-
     jshint: {
       options: {
         curly: true,
@@ -20,37 +19,49 @@ module.exports = function(grunt) {
         boss: true,
         eqnull: true,
         node: true,
-        es5: true
-      }
+        es5: true,
+      },
+    },
+    nodeunit: {
+      tasks: ['test/*_test.js']
     },
 
+    // Configuration to be run (and then tested).
     less: {
       compile: {
         files: {
-          "tmp/less_a.css": ["test/fixtures/style.less"],
-          "tmp/less_b.css": ["test/fixtures/style.less"],
-          "tmp/less_c.css": ["test/fixtures/**/*.nomatches"],
-          "tmp/less_d.css": ["test/fixtures/style.less", "test/fixtures/style2.less"]
+          '<%= TMP %>/less_a.css': ['test/fixtures/style.less'],
+          '<%= TMP %>/less_b.css': ['test/fixtures/style.less'],
+          '<%= TMP %>/less_c.css': ['test/fixtures/**/*.nomatches'],
+          '<%= TMP %>/less_d.css': ['test/fixtures/style.less', 'test/fixtures/style2.less']
         },
         options: {
-          paths: ["test/fixtures/include"]
-        }
+          paths: ['test/fixtures/include']
+        },
       }
     },
-
-    nodeunit: {
-      tasks: ["test/*_test.js"]
-    }
-
   });
 
-  grunt.registerTask("testprep", "prepare for tests", function() {
-    require("rimraf").sync("tmp");
-    grunt.file.mkdir("tmp");
+  // This needs to be run before our task + tests are run.
+  grunt.registerTask('test_setup', 'Test setup.', function() {
+    // Create a temporary directory.
+    var Tempdir = require('temporary/lib/dir');
+    var tmpdir = new Tempdir();
+    // Update config so our task can utilize the tempdir.
+    grunt.config.set('TMP', tmpdir.path);
+    // Unit tests need to know where the tempdir is.
+    global.TMP = tmpdir.path;
   });
 
-  grunt.registerTask("default", "lint");
-  grunt.renameTask("test", "nodeunit");
-  grunt.registerTask("test", "testprep less nodeunit");
-  grunt.loadTasks("tasks");
+  // Whenever "test" is run, perform test setup, run task in as many ways
+  // as necessary, then test the result.
+  grunt.renameTask('test', 'nodeunit');
+  grunt.registerTask('test', 'test_setup less nodeunit');
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', 'lint test');
+
+  // Actually load tasks to be tested.
+  grunt.loadTasks('tasks');
+
 };
