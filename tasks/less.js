190b19992bba01bf253edef4b0c6ff23450e344e
Ben Alman <cowboy@rj3.net>
2012-09-06 12:03:04 -0400

    Normalizing quotes to single.

--- tasks/less.js
+++ tasks/less.js
@@ -8,7 +8,7 @@
  */
 
 module.exports = function(grunt) {
-  "use strict";
+  'use strict';
 
   // TODO: ditch this when grunt v0.4 is released
   grunt.util = grunt.util || grunt.utils;
@@ -21,11 +21,11 @@ module.exports = function(grunt) {
   var lessError = function(e) {
     var pos = '[' + 'L' + e.line + ':' + ('C' + e.column) + ']';
     grunt.log.error(e.filename + ': ' + pos + ' ' + e.message);
-    grunt.fail.warn("Error compiling LESS.", 1);
+    grunt.fail.warn('Error compiling LESS.', 1);
   };
 
   var less = function(source, options, callback) {
-    require("less").Parser(options).parse(source, function(parse_error, tree) {
+    require('less').Parser(options).parse(source, function(parse_error, tree) {
       if (parse_error) {
         lessError(parse_error);
       }
@@ -39,10 +39,10 @@ module.exports = function(grunt) {
     });
   };
 
-  grunt.registerMultiTask("less", "Compile LESS files to CSS", function() {
+  grunt.registerMultiTask('less', 'Compile LESS files to CSS', function() {
     var options = helpers.options(this);
 
-    grunt.verbose.writeflags(options, "Options");
+    grunt.verbose.writeflags(options, 'Options');
 
     // TODO: ditch this when grunt v0.4 is released
     this.files = helpers.normalizeMultiTaskFiles(this.data, this.target);
@@ -64,8 +64,8 @@ module.exports = function(grunt) {
           nextConcat(null, css);
         });
       }, function(err, css) {
-        grunt.file.write(file.dest, css.join("\n") || "");
-        grunt.log.writeln("File '" + file.dest + "' created.");
+        grunt.file.write(file.dest, css.join('\n') || '');
+        grunt.log.writeln('File "' + file.dest + '" created.');
 
         next();
       });
