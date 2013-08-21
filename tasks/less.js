8e225e3355a36c920dfc31f5229e0791d93c96af
Matthew Tretter <m@tthewwithanm.com>
2013-08-21 14:50:38 -0400

    Support "strictUnits" option. Closes GH-72.
    Conflicts:
    	tasks/less.js

--- tasks/less.js
+++ tasks/less.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
 
   var lessOptions = {
     parse: ['paths', 'optimization', 'filename', 'strictImports', 'syncImport', 'dumpLineNumbers', 'relativeUrls', 'rootpath'],
-    render: ['compress', 'cleancss', 'ieCompat', 'strictMath']
+    render: ['compress', 'cleancss', 'ieCompat', 'strictMath', 'strictUnits']
   };
 
   grunt.registerMultiTask('less', 'Compile LESS files to CSS', function() {
