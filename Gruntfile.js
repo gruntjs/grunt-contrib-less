3e0ada7529dc9872b70332fc03514ae57d688918
XhmikosR <xhmikosr@gmail.com>
2014-11-20 08:25:24 +0200

    Add `jshint` in test target.

--- Gruntfile.js
+++ Gruntfile.js
@@ -254,9 +254,9 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'less', 'nodeunit']);
+  grunt.registerTask('test', ['jshint', 'clean', 'less', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test', 'build-contrib']);
+  grunt.registerTask('default', ['test', 'build-contrib']);
 
 };
