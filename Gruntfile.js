38ef82e938e7eee203d4e7910c876f9af6499ba8
Kyle Robinson Young <kyle@dontkry.com>
2015-01-01 11:31:03 -0800

    Update copyright to 2015

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@
  * grunt-contrib-less
  * http://gruntjs.com/
  *
- * Copyright (c) 2014 Tyler Kellen, contributors
+ * Copyright (c) 2015 Tyler Kellen, contributors
  * Licensed under the MIT license.
  */
 
