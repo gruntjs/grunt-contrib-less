0790ab5739a3d9ce7e0d560b715be67c9297ee3d
Chris Montoro <christopher.montoro@nytimes.com>
2016-03-22 21:40:39 -0400

    accept a function for sourceMapFilename

--- Gruntfile.js
+++ Gruntfile.js
@@ -146,6 +146,19 @@ module.exports = function(grunt) {
         src: 'test/fixtures/style3.less',
         dest: 'tmp/sourceMapFilename.css'
       },
+      sourceMapFilenameFunction: {
+        options: {
+          sourceMap: true,
+          sourceMapFilename: function (dest) {
+            var path = require('path');
+            var destPath = path.parse(dest);
+            return path.join(destPath.dir, 'maps', destPath.base + '.map');
+            // returns 'tmp/maps/sourceMapFilename.css.map';
+          }
+        },
+        src: 'test/fixtures/style3.less',
+        dest: 'tmp/sourceMapFilename.css'
+      },
       sourceMapURL: {
         options: {
           sourceMap: true,
